528aae93cf50bf88adfb0f76f34a67a903d13040
Matthammond96 <ultramatt96@hotmail.co.uk>
2024-04-26 15:00:39 +0200

    Add support for writing to file

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,15 @@
-**Install required packages**
+This package decodes base58 strings to binary arrays. Base58 is commonly used when encoding Solana KeyPairs, however, some apps only support binary arrays. Recently added support for optional writing the output to a path.
+
+### Install required packages
+
 `npm install`
 
-**Decode your encoded string**
-`npm run decode exampleEncodeString`
+### Decoding your encoded string
+
+`npm run decode exampleEncodedString`
+
+### Decoding your encoded string and save to file
+
+`npm run decode exampleEncodedString decoded.txt`
+
+_Please note when setting a path the script will recursively check for the directories existence and will automatically create the directory if the location does not exist._
--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 #!/usr/bin/env node
+import fs from "fs";
 import { base58_to_binary } from "base58-js";
 
-const bin = base58_to_binary(process.argv[2]);
-console.log([...bin]);
+const [, , encodedString, outputPath] = process.argv;
+
+/**
+ * @param {string} encodedString
+ * @param {string} outputPath
+ */
+function main(encodedString, outputPath) {
+  if (!encodedString) return console.error("Error: No encoded string found.");
+
+  const binaryResponse = [...base58_to_binary(encodedString)];
+
+  if (!outputPath) return console.log([...binaryResponse]);
+
+  try {
+    // Check if folders exist and create when not to avoid writeFile errors
+    const folderPath = outputPath.match("(.+)/([^/]+)");
+    if (folderPath) {
+      const folders = folderPath[1].split("/");
+
+      let dir = "";
+
+      for (let folder of folders) {
+        dir = `${dir}${folder}/`;
+        if (!fs.existsSync(dir)) {
+          fs.mkdirSync(dir);
+        }
+      }
+    }
+
+    fs.writeFileSync(outputPath, Buffer.from(`[${binaryResponse.join(", ")}]`));
+
+    console.log(`Successfully decoded and saved to ${outputPath}`);
+  } catch (err) {
+    if (err) return console.error(`${err}`);
+  }
+}
+
+main(encodedString, outputPath);
